6299d964eadc5cd5e38b8cf98dbe46a745c33dd5
DavidFermer <david.fermer@icloud.com>
2014-11-06 19:18:26 -0500

    changed conditional to store the answer and added a seperate console log to output the final statement

--- sdi-lab5/fermer_david_lab5.js
+++ sdi-lab5/fermer_david_lab5.js
@@ -33,28 +33,34 @@ var divide = function(num1,num2)
 };
 
 //Main Code
+//  This is my prompts that gather the numbers and the type of math the user would like to do.
 number1 = parseInt(prompt("Enter your first whole number:"));
 number2 = parseInt(prompt("Enter your second number:"));
 operation = prompt("What operation would you like to apply to the numbers?","add, subtract, multiply or divide");
 
+//  This code makes the user input something other than 0 for the second number if dividing
 if ((number2 == 0) && (operation == "divide"))
 	{
 		number2 = parseInt(prompt("You cannot divide a number by 0.  Please input a number besides 0."));
 	}
-
+	
+//  This set of conditionals calls my functions and stores the equation to the variable answer
 if (operation == "add")
 {
-	console.log(number1 + " + " + number2 + " = " + add(number1,number2));
+	answer = add(number1,number2);
 }
 else if (operation == "subtract")
 {
-	console.log(number1 + " - " + number2 + " = " + subtract(number1,number2));
+	answer = subtract(number1,number2);
 }
 else if (operation == "multiply")
 {
-	console.log(number1 + " * " + number2 + " = " + multiply(number1,number2));
+	answer = multiply(number1,number2);
 }
 else if (operation == "divide")
 {
-	console.log(number1 + " / " + number2 + " = " + divide(number1,number2));
+	answer = divide(number1,number2);
 }
+
+//This code logs the operation, both numbers and the answer
+console.log("If you " + operation + " " + number1 + " and " + number2 + " you get " + answer + ".");
