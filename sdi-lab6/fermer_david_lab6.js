ad4be8cd093d969e7e86e731e417fe307662d45e
DavidFermer <david.fermer@icloud.com>
2014-11-08 17:19:08 -0500

    first commit for lab6

---
+++ sdi-lab6/fermer_david_lab6.js
@@ -0,0 +1,26 @@
+//alert("JavaScript works!");
+
+// David G. Fermer
+// SDI 1411
+// Lab 6 Working with Arrays
+
+//variables
+
+//functions
+var procedure = function()
+{
+	var myFamilyNames 		= ["David", "Regina", "James", "Bradley", "Benjamin"];
+	var myFamilyHairColor 	= ["Brown", "Blonde", "Blonde", "Blonde", "Not yet know"];
+	var name = 0;
+	var hairColor = 0;
+	
+	for (i = 0; i < myFamilyNames.length; i++)
+	{
+		name = myFamilyNames[i];
+		console.log(name);		
+	};
+};
+
+
+//main code
+procedure();
