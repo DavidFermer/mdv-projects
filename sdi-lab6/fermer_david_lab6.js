57fdf130d8e03f92b06d3772034df2ef214a4227
DavidFermer <david.fermer@icloud.com>
2014-11-08 18:29:36 -0500

    lab 6 commit 4, added flow chart and made minor corrections to code

--- sdi-lab6/fermer_david_lab6.js
+++ sdi-lab6/fermer_david_lab6.js
@@ -9,18 +9,34 @@
 //functions
 var procedure = function()
 {
-	var myFamilyNames 		= ["David", "Regina", "James", "Bradley", "Benjamin"];
-	var myFamilyHairColor 	= ["Brown", "Blonde", "Blonde", "Blonde", "Not yet know"];
-	var name = 0;
-	var hairColor = 0;
+	var myFamilyNames 		= ["David", "Regina", "James", "Bradley"];
+	var myFamilyHairColors 	= ["Brown", "Blonde", "Blonde", "Blonde"];
 	
 	for (i = 0; i < myFamilyNames.length; i++)
 	{
-		name = myFamilyNames[i];
-		console.log(name);		
+		if (myFamilyNames[i] == "David")
+		{
+			console.log("My name is " + myFamilyNames[i] + " and my hair color is " + myFamilyHairColors[i] + ".");
+		}
+		
+		else if (myFamilyNames[i] == "Regina")
+		{
+			console.log("My wife's name is " + myFamilyNames[i] + " and her hair color is " + myFamilyHairColors[i] + ".");
+		}
+		
+		else 
+		{
+			console.log(myFamilyNames[i] + " is one of my sons and his hair color is " + myFamilyHairColors[i] + ".");
+		}
+		
 	};
+	
+	myFamilyNames.push("Benjamin");
+	
+	myFamilyHairColors.push("not yet know");
+	
+	console.log("I have another son on the way.  His name is " + myFamilyNames[4] + " and since he has not yet been born we do " + myFamilyHairColors[4] + " what his hair color is.");
 };
-
 
 //main code
 procedure();
