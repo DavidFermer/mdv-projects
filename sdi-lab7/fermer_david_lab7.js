e70bb89e4aa9a443c108550772a1acc9b93c3a3e
DavidFermer <david.fermer@icloud.com>
2014-11-11 19:36:45 -0500

    lab 7 2nd commit added constructor.

--- sdi-lab7/fermer_david_lab7.js
+++ sdi-lab7/fermer_david_lab7.js
@@ -1,1 +1,31 @@
-alert("JavaScript works!");
+//David Fermer
+// SDI 1411
+// Lab 7
+
+//Global Variables
+var stormtrooper;
+var sentry;
+var officer;
+
+//Functions
+var badGuyConstructor = function(npcType,npcHealth,npcWeapon,npcWeaponBaseDamage)
+{
+	this.type 				= npcType;
+	//console.log("npc type " + this.type);
+	this.health 			= npcHealth;
+	//console.log("npc health " + this.health);
+	this.weapon				= npcWeapon;
+	//console.log("npc weapon " + this.weapon);
+	this.baseDamage			= npcWeaponBaseDamage;
+	//console.log("weapon Base Damage " + this.baseDamage);
+	this.alive				= false;
+	//console.log("the npc is alive " + this.alive);
+	this.returnFromDeath 	= function()
+	{
+		this.alive = true;
+	};
+	
+	this.returnFromDeath();
+	
+	//console.log("the npc is now alive " + this.alive);
+};
