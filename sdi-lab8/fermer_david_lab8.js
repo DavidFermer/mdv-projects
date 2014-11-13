d3852d8483d140a36857903e316b8ef91fdbe2f7
DavidFermer <david.fermer@icloud.com>
2014-11-13 17:24:34 -0500

    Lab 8, first commit. Just starting out.

---
+++ sdi-lab8/fermer_david_lab8.js
@@ -0,0 +1,28 @@
+//David G. Fermer
+// SDI 1411
+// Lab 8
+
+//Globral Variables
+
+
+//Functions
+var emailBooleanCheck =function(emailAddress)
+{
+	var emailChecker 	= false;
+	var atSymbolCheck 	= false;
+	var numberOfAts		=0;
+	
+	for(i = 0; i < emailAddress.length; i++)
+	{
+		//console.log(i);
+		numberofAts = emailAddress
+	}
+	
+	
+	
+};
+
+//Main Code
+
+
+emailBooleanCheck("david.fermer@icloud.com")
