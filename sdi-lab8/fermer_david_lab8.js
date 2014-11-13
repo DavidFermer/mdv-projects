4e859fe28595e2dda7c08d5138fcf3bd934e2db0
DavidFermer <david.fermer@icloud.com>
2014-11-13 18:00:18 -0500

    Lab 8, second commit.  Just completed problem 1.  Now working on flow chart for problem 1

--- sdi-lab8/fermer_david_lab8.js
+++ sdi-lab8/fermer_david_lab8.js
@@ -3,26 +3,48 @@
 // Lab 8
 
 //Globral Variables
-
+var firstProblemAnswer;
 
 //Functions
-var emailBooleanCheck =function(emailAddress)
+var emailBooleanCheck = function(emailAddress)
 {
 	var emailChecker 	= false;
-	var atSymbolCheck 	= false;
-	var numberOfAts		=0;
-	
-	for(i = 0; i < emailAddress.length; i++)
+	var firstAt			= emailAddress.indexOf("@");
+	//console.log(firstAt);
+	var anyOtherAt		= emailAddress.indexOf("@", firstAt + 1)
+	//console.log(anyOtherAt);
+	var spaceCheck		= emailAddress.indexOf(" ");
+	//console.log(spaceCheck);
+	var dotCheck		= emailAddress.indexOf(".",firstAt);
+	//console.log(dotCheck);
+
+	if(spaceCheck > 0)
 	{
-		//console.log(i);
-		numberofAts = emailAddress
+		return false;
 	}
-	
-	
-	
+	else if (firstAt < 0)
+	{
+		return false;
+	}
+	else if(dotCheck < firstAt)
+	{
+		return false;
+	}
+	else if(anyOtherAt != -1)
+	{
+		return false;
+	}
+	else
+	{
+		return true;	
+	}
+
+
 };
+	
 
 //Main Code
 
 
-emailBooleanCheck("david.fermer@icloud.com")
+var firstProblemAnswer = emailBooleanCheck("david.fermer@icloud.com")
+console.log(firstProblemAnswer) 
